9a2f300ca28f3e244a7c11d8e197cbc36fcc3b9e
NVE <nve.data@outlook.com>
2024-08-04 12:36:26 +0200

    Use get_parquet_stats

--- parquet_size.py
+++ parquet_size.py
@@ -1,3 +1,4 @@
+from functools import reduce
 import os
 from pathlib import Path
 
@@ -29,18 +30,43 @@ def print_parquet_stats(dir_name: str) -> None:
         df_part.groupby('id').agg(F.count('id')).show()
 
 
+def get_parquet_stats(dir_name: str) -> list[DataFrame]:
+
+    dir_path = Path(dir_name)
+    parquet_paths = dir_path.glob('**/*.parquet')
+    parquet_paths
+    stats = []
+    for path in parquet_paths:
+        stat_df : DataFrame = spark.createDataFrame([{
+            'dir_name': dir_name,
+            'part_name': path.name.split('-')[1],
+            'file_size': os.path.getsize(path)
+        }])
+
+        part_df = spark.read.parquet(str(path))
+        groups_df = part_df.groupby('id').agg(
+            F.count('id').alias('count')
+        )
+        stat_df = stat_df.join(groups_df, F.lit(True), how='left')
+        stats.append(stat_df)
+
+    return stats
+
+
 spark: SparkSession = SparkSession.builder.master("local").getOrCreate()
 
-df = create_rand_df(10**7, 2)
+stats = []
+df = create_rand_df(10**6, 2)
 df = df.repartition(2, 'id')
 dir_name = 'repart_2_and_col'
 df.write.parquet(dir_name, mode='overwrite')
-print(dir_name)
-print_parquet_stats(dir_name)
+stats.extend(get_parquet_stats(dir_name))
 
-df = create_rand_df(10**7, 2)
+df = create_rand_df(10**6, 2)
 df = df.repartition(2)
 dir_name = 'repart_2'
 df.write.parquet(dir_name, mode='overwrite')
-print(dir_name)
-print_parquet_stats(dir_name)
+stats.extend(get_parquet_stats(dir_name))
+
+stats_df = reduce(DataFrame.union, stats)
+stats_df.orderBy('dir_name', 'part_name', 'id').show()
