df1a58dc58002f1206a8f28a7b2371c20bacb7f4
NVE <nve.data@outlook.com>
2024-08-04 11:37:15 +0200

    Repartition wtih number only

--- parquet_size.py
+++ parquet_size.py
@@ -1,22 +1,46 @@
 import os
 from pathlib import Path
 
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.types import IntegerType
 
+def create_rand_df(
+    n_rows: int,
+    n_unique: int
+    ) -> DataFrame:
+
+    df = spark.range(n_rows)
+    df = df.withColumn(
+        "id", 
+        F.round(
+            F.rand() * (n_unique - 1)
+        ).cast(IntegerType())
+    )
+    return df
+
+
+def print_parquet_stats(dir_name: str) -> None:
+    dir_path = Path(dir_name)
+    parquet_paths = dir_path.glob('**/*.parquet')
+    for path in parquet_paths:
+        print('file size:', os.path.getsize(path))
+        df_part = spark.read.parquet(str(path))
+        df_part.groupby('id').agg(F.count('id')).show()
+
+
 spark: SparkSession = SparkSession.builder.master("local").getOrCreate()
 
-df = spark.range(10**7)
-df = df.withColumn("id", F.round(F.rand()).cast(IntegerType()))
-
+df = create_rand_df(10**7, 2)
 df = df.repartition(2, 'id')
 dir_name = 'repart_2_and_col'
 df.write.parquet(dir_name, mode='overwrite')
+print(dir_name)
+print_parquet_stats(dir_name)
 
-dir_path = Path(dir_name)
-parquet_paths = dir_path.glob('**/*.parquet')
-for path in parquet_paths:
-    print('file size:', os.path.getsize(path))
-    df_part = spark.read.parquet(str(path))
-    df_part.groupby('id').agg(F.count('id')).show()
+df = create_rand_df(10**7, 2)
+df = df.repartition(2)
+dir_name = 'repart_2'
+df.write.parquet(dir_name, mode='overwrite')
+print(dir_name)
+print_parquet_stats(dir_name)
