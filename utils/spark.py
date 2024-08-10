1ca634589096726578be267b6bbc447469432721
NVE <nve.data@outlook.com>
2024-08-10 16:19:37 +0200

    Add annotations_to_schema

--- utils/spark.py
+++ utils/spark.py
@@ -1,8 +1,20 @@
-from pyspark.sql.types import DataType, StringType, FloatType, IntegerType
+import inspect
+from pyspark.sql.types import DataType, StringType, FloatType, IntegerType, StructType, StructField
 
 def to_pyspark_type(py_type: type) -> DataType:
+    
     # explicit equality comparison, unlike in match-case
     if py_type == int: return IntegerType
     elif py_type == float: return FloatType
     elif py_type == str: return StringType
     else: raise TypeError('unable to map the type')
+
+
+def annotations_to_schema(dataclass) -> StructType:
+
+    annotations = inspect.get_annotations(dataclass)
+    schema = StructType([
+        StructField(name, to_pyspark_type(type_)())
+        for name, type_ in annotations.items()
+    ])
+    return schema
