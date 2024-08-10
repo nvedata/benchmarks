ea3675e3ed187a7728713a983e722eb4779a4bdb
NVE <nve.data@outlook.com>
2024-08-10 16:45:52 +0200

    More Python types

--- utils/spark.py
+++ utils/spark.py
@@ -1,20 +1,22 @@
 import inspect
-from pyspark.sql.types import DataType, StringType, FloatType, IntegerType, StructType, StructField
+from pyspark.sql import types as T
 
-def to_pyspark_type(py_type: type) -> DataType:
-    
+def to_pyspark_type(py_type: type) -> T.DataType:
+
     # explicit equality comparison, unlike in match-case
-    if py_type == int: return IntegerType
-    elif py_type == float: return FloatType
-    elif py_type == str: return StringType
+    if py_type == bool: return T.BooleanType
+    elif py_type == int: return T.IntegerType
+    elif py_type == float: return T.FloatType
+    elif py_type == str: return T.StringType
+    elif py_type == list: return T.ArrayType 
     else: raise TypeError('unable to map the type')
 
 
-def annotations_to_schema(dataclass) -> StructType:
+def annotations_to_schema(dataclass) -> T.StructType:
 
     annotations = inspect.get_annotations(dataclass)
-    schema = StructType([
-        StructField(name, to_pyspark_type(type_)())
+    schema = T.StructType([
+        T.StructField(name, to_pyspark_type(type_)())
         for name, type_ in annotations.items()
     ])
     return schema
