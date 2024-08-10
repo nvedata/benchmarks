13e293d857650311b5e212c227668304de24b7d8
NVE <nve.data@outlook.com>
2024-08-10 18:44:37 +0200

    Use Point

--- parquet_size.py
+++ parquet_size.py
@@ -1,11 +1,48 @@
+from dataclasses import dataclass
 import itertools
 from functools import reduce
 import os
 from pathlib import Path
+from typing import Iterable
 
 from pyspark.sql import SparkSession, DataFrame, Column, DataFrameWriter
 from pyspark.sql import functions as F
 from pyspark.sql.types import IntegerType
+
+from utils.spark import annotations_to_schema
+
+
+@dataclass
+class Point:
+    '''Point of parameters.
+
+    Notes
+    -----
+    Attribute annotations define Spark DataFrame schema.
+    Handle with care.
+    '''
+    n_rows: int
+    fractions: list[int | float]
+    part_mode: str
+    n_part: int | None
+    part_cols: list[str] | None
+    # buckets: list[int, list[str]]
+    # sort_cols: list[str] | None
+
+
+class Dimensions(Point):
+
+    n_rows: list[int]
+    fractions: list[list[int | float]]
+    part_mode: list[str]
+    n_part: list[int | None]
+    part_cols: list[list[str] | None]
+    # buckets: list[list[int, list[str]]]
+    # sort_cols: list[list[str] | None]
+
+    @property
+    def grid_iterator(self) -> Iterable:
+        return itertools.product(*vars(self).values())
 
 
 class PartitioningCase:
@@ -71,7 +108,7 @@ def create_rand_df(
 
 def create_skewed_df(
     n_rows: int,
-    fractions: list[float]
+    fractions: list[int | float]
     ) -> DataFrame:
 
     spark = SparkSession.getActiveSession()
@@ -115,7 +152,9 @@ def partitioning(
     ) -> DataFrame:
 
     if mode == 'repartition':
-        if n_part is None:
+        if n_part is None and cols is None:
+            df = df
+        elif n_part is None:
             df = df.repartition(*cols)
         elif cols is None:
             df = df.repartition(n_part)
@@ -124,7 +163,10 @@ def partitioning(
         return df
     
     elif mode == 'coalesce':
-        df = df.coalesce(n_part)
+        if n_part is None:
+            df = df
+        else:
+            df = df.coalesce(n_part)
         return df
 
     else:
@@ -137,7 +179,8 @@ def partitioning(
 def get_bucketing_writer(
     df: DataFrame,
     n_buckets: int = None,
-    bucket_cols: list[str] = None
+    bucket_cols: list[str] = None,
+    sort_cols: list[str] = None
     ) -> DataFrameWriter:
 
     if n_buckets is None != bucket_cols is None:
@@ -147,30 +190,63 @@ def get_bucketing_writer(
         )
     
     if n_buckets is None:
-        return df.write
+        return  df.write
     else:
-        return df.write.bucketBy(n_buckets, *bucket_cols)
+        writer = df.write.bucketBy(n_buckets, *bucket_cols)
+
+    if sort_cols is not None:
+        writer = writer.sortBy(sort_cols)
+
+    return writer
 
 
 def set_writer_sorting(
     writer: DataFrameWriter,
-    sorting_cols: list[str] = None
-    ) -> DataFrame:
+    sort_cols: list[str] = None
+    ) -> DataFrameWriter:
 
-    if sorting_cols is None:
+    if sort_cols is None:
         return writer
     else:
-        writer.sortBy()
+        writer.sortBy(sort_cols)
 
 
-def get_case_stat(params: dict) -> DataFrame:
+def get_case_stat(params: Point, dir_name: str) -> DataFrame:
 
     spark = SparkSession.getActiveSession()
-    stats_df = get_parquet_stats(self.name)
-    case_df : DataFrame = spark.createDataFrame([params])
+    stats_df = get_parquet_stats(dir_name)
+    params_f= format_params(params)
+    schema = annotations_to_schema(Point)
+    case_df : DataFrame = spark.createDataFrame(params_f, schema=schema)
     case_df = case_df.join(stats_df, F.lit(True))
     
     return case_df
+
+
+def format_params(params: Point) -> tuple[dict[str, object]]:
+    '''Format parameters values.
+
+    Parameters
+    ----------
+    params: Point
+        Point of parameters.
+
+    Returns
+    -------
+    dict[str, object]
+        Formatted parameters.
+    '''
+
+    f_params = {}
+    for key, val in vars(params).items():
+        # if val is None:
+        #     val = 'NONE'
+        if isinstance(val, list):
+            val = str(val)
+
+        f_params[key] = val
+
+    return f_params
 
 
 def print_parquet_stats(dir_name: str) -> None:
@@ -214,6 +290,7 @@ def main():
     spark: SparkSession = SparkSession.builder.master("local").getOrCreate()
 
     # TODO grids sum
+    # TODO grid subtraction
     dimensions = {
         "n_rows": [
             10 ** 6,
@@ -231,27 +308,32 @@ def main():
         "part_cols": [
             None,
             ["id"]
-        ],
-        "buckets" : [
-            [1, "id"],
-            [2, "id"],
-            [None, None]
-        ],
-        "sort_cols": [
-            ["id", None]
         ]
     }
 
     stats = []
-    for values in itertools.product(*dimensions.values()):
-        params = {key: val for key, val in zip(dimensions.keys(), values)}
-        print(params)
+    # for values in itertools.product(*dimensions.values()):
+    #     params = {key: val for key, val in zip(dimensions.keys(), values)}
+    #     print(params)
         # df = create_skewed_df(n_rows, fractions)
         # df = partitioning(df, mode, n_part, cols)
         # writer = get_bucketing_writer(df, n_buckets, bucket_cols)
         # writer = set_writer_sorting(writer, sort_cols)
         # stats = get_case_stat(df, params)
 
+    dims = Dimensions(**dimensions)
+    # TODO enumerate starting from max dir_name for append mode
+    # TODO grid subtraction to exclude existing parameters
+    for i, point in enumerate(dims.grid_iterator):
+        dir_name = str(i)
+        p = Point(*point)
+        print(vars(p))
+        # df = create_skewed_df(p.n_rows, p.fractions)
+        # df = partitioning(df, p.part_mode, p.n_part, p.part_cols)
+        # # bucketing is not supported for parquet
+        # # writer = get_bucketing_writer(df, *p.buckets, p.sort_cols)
+        # df.write.parquet(dir_name, mode='overwrite')
+        # stats = get_case_stat(p, dir_name)
     
     cases = [
         PartitioningCase("repart_1m_p2", 10**6, 2, n_part=2),
@@ -267,8 +349,9 @@ def main():
         stats_df = case_.run()
         stats.append(stats_df)
 
-    stats_df = reduce(DataFrame.union, stats)
-    stats_df.orderBy("dir_name", "part_name", "id").show()
+    report = reduce(DataFrame.union, stats)
+    report.orderBy("dir_name", "part_name", "id").show()
+    # TODO write report to csv with append option
 
     # TODO parquet cleanup option
 if __name__ == "__main__":
